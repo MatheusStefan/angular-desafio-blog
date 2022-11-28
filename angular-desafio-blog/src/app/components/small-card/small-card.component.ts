91445c4544a47d45522f431b180f10b8bde6f9d9
MatheusStefan <roccomatheus0@gmail.com>
2022-11-28 18:00:18 -0300

    Estrutura de componentes dinâmicos

--- angular-desafio-blog/src/app/components/small-card/small-card.component.ts
+++ angular-desafio-blog/src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SmallCardComponent implements OnInit {
 
+  @Input()
+  image:string = ""
+  @Input()
+  smallCardTitle:string = ""
+ 
   constructor() { }
 
   ngOnInit(): void {
